f297091a691835b102a707a1a00c4633a6a99415
ssneller <ssneller@Steves-MacBook-Pro.local>
2016-12-03 20:00:20 -0800

    3rd commit with connections on SigninVC

--- SigninVC.swift
+++ SigninVC.swift
@@ -9,6 +9,13 @@
 import UIKit
 
 class SigninVC: UIViewController {
+    
+    @IBOutlet var emailTextField: UITextField!
+    
+    @IBOutlet var passwordTextField: UITextField!
+    
+    @IBOutlet var phoneTextField: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,20 +23,15 @@ class SigninVC: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func logIn(_ sender: Any) {
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func signUp(_ sender: Any) {
     }
-    */
+    
+    
 
-}
+
+}   // class
+
